3ff3abfaadc97247239d7a6b7a63d19246d762e6
DESKTOP-1F2LFR5\SDiego <sev17238@uvg.edu.gt>
2018-04-18 08:59:10 -0600

    Laboratorio3 terminado :D

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,31 +7,42 @@ public class Character : MonoBehaviour {
 
     Rigidbody2D rb2d;
     SpriteRenderer sr;
+    public Animator anim;
+    public AudioSource audio;
+
     public Camera cam;
     private float speed = 5f;
     private float jumpForce = 250f;
     private bool facingRight = true;
+
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+        audio = GetComponent<AudioSource>();
+
+        anim = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         float move = Input.GetAxis("Horizontal");
+        
         if (move != 0) {
             rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
             cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
             facingRight = move > 0;
+
+            anim.SetFloat("Speed", Mathf.Abs(move));
         }
         
         sr.flipX = !facingRight;
 
-        if (Input.GetButtonDown("Jump")) {
+        if (/*Input.GetButtonDown("Jump")*/Input.GetKeyDown(KeyCode.UpArrow)) { 
             rb2d.AddForce(Vector2.up*jumpForce);
+            audio.Play();
         }
 
 	}
